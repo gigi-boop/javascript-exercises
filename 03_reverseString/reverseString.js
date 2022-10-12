2424023fbe15d7f0afdba3f67ad78efe9eea4b8d
gigi-boop <agentile89@protonmail.com>
2022-10-11 21:49:24 -0400

    Completed exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (string) {
+    const splitString = string.split("");
+    const reversedArray = [];
 
+    for (let i = 1; i <= splitString.length; i++) {
+        reversedArray.push(splitString[splitString.length - i]);
+    }
+
+    return reversedArray.join("");
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...argumentsToBeRemoved) {
+    let index = 0;
 
+    for (i = 0; i < array.length && index < argumentsToBeRemoved.length; ) {
+        if (array[i] !== argumentsToBeRemoved[index]) {
+            i++;
+        } else if (array[i] === argumentsToBeRemoved[index]) {
+            array.splice(i, 1);
+            index++;
+            i = 0;
+        }
+
+        if (
+            array[i] === array[array.length - 1] &&
+            array[i] !== argumentsToBeRemoved[index]
+        ) {
+            index++;
+            i = 0;
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
