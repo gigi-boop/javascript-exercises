41f62aea5a84769eb91499c828aff7d9642f3fe1
gigi-boop <agentile89@protonmail.com>
2022-10-13 00:32:24 -0400

    Fix not declaring variables

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,10 @@
 const ftoc = function (temp) {
-    celsius = ((temp - 32) * 5) / 9;
+    const celsius = ((temp - 32) * 5) / 9;
     return Math.round(celsius * 10) / 10;
 };
 
 const ctof = function (temp) {
-    fahrenheit = (temp * 9) / 5 + 32;
+    const fahrenheit = (temp * 9) / 5 + 32;
     return Math.round(fahrenheit * 10) / 10;
 };
 
